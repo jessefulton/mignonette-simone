4864f7f4dfaf75d4f1fda538e27e3bde5f248ac5
jessefulton.com <jessefulton.com@9f913013-436c-0410-b08b-82dccf8bc586>
2010-02-17 04:58:13 +0000

    cleanup - ready for release
    git-svn-id: svn+ssh://jessefulton.com/home/66721/data/svn/repository/projects/mignonette@143 9f913013-436c-0410-b08b-82dccf8bc586

--- simone/simone.c
+++ simone/simone.c
@@ -19,6 +19,13 @@
  *
  *	revision history:
  *
+ *  - Feb 16, 2010 - jesse
+ *      cleaning up code for "official" release
+ *
+ *  - Jan 16, 2010 - jesse
+ *      added more music & sound effects
+ *      added random number generator
+ *
  *	- Jan 13, 20010 - jesse
  *		created. (Used "Munch" as a template)
  *
@@ -44,76 +51,16 @@
 #include "miggl.h"		/* Mignonette Game Library */
 
 
-/* audio stuff - beware, these song tables take up much memory! (so comment out unused tables) */
+
+//=============================================================
+// Time functions by Rolf Van Widenfelt & Mitch Altman (munch)
+//=============================================================
 
 
-//
-// a few notes from a well known classical melody (J.S. Bach, Cantata BWV 147)
-//
-static byte IntroSong[] = {
-N_C4,N_8TH,
-N_E4,N_8TH,
-N_F4,N_HALF,
-N_REST,N_QUARTER,
-N_C4,N_8TH,
-N_E4,N_8TH,
-N_G4,N_HALF,
-N_REST,N_QUARTER,
-N_F4,N_8TH,
-N_E4,N_8TH,
-N_C4,N_HALF,
-N_END,
-};
-
-
-static byte TapsSong[] = {
-N_G3, N_HALF,
-N_G3, N_8TH,
-N_C4, N_WHOLE,
-N_G3, N_HALF,
-N_C4, N_8TH,
-N_E4, N_WHOLE,
-N_END
-};
-
-
-static byte DIRECTION_A_NOISE[] = {N_F4, N_16TH, N_END};
-static byte DIRECTION_B_NOISE[] = {N_D4, N_16TH, N_END};
-static byte DIRECTION_C_NOISE[] = {N_E4, N_16TH, N_END};
-static byte DIRECTION_D_NOISE[] = {N_G4, N_16TH, N_END};
-//static byte BUTTON_NOISE[] = {N_C5, N_16TH, N_END};
-static byte CORRECT_NOISE[] = {N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, N_END};
-static byte WIN_NOISE[] = {
-N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
-N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
-N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
-N_END};
-
-
-/* colors */
-#define DIRECTION_A 0
-#define DIRECTION_B	1
-#define DIRECTION_C	2
-#define DIRECTION_D	3
-
-
-static byte directions[4] = {
-	DIRECTION_A,
-	DIRECTION_B,
-	DIRECTION_C,
-	DIRECTION_D
-};
-
-static byte arrows[100];
-//static byte[100] colors;
-//static byte[100] sounds;
-
-
-//
-// crude delay of 1 to 255 us
-//
-void
-delay_us(byte usec)
+/**
+ * crude delay of 1 to 255 us
+ */
+void delay_us(byte usec)
 {
 	usec++;
 	
@@ -123,13 +70,10 @@ delay_us(byte usec)
 }
 
 
-//
-// crude delay of 1 to 255 ms
-//
-void
-delay_ms(uint8_t ms)
-{
-	
+/**
+ * crude delay of 1 to 255 ms
+ */
+void delay_ms(uint8_t ms) {
 	ms++;
 	
 	while (--ms) {
@@ -138,12 +82,10 @@ delay_ms(uint8_t ms)
 }
 
 
-//
-// crude "sleep" function for 0 to 255 seconds
-//
-void
-delay_sec(uint8_t sec)
-{
+/**
+ * crude delay of 1 to 255 s
+ */
+void delay_sec(uint8_t sec) {
 	uint8_t i;
 	for (i = 0; i < sec; i++) {
 		delay_ms(250);
@@ -154,24 +96,17 @@ delay_sec(uint8_t sec)
 }
 
 
-#define XMAX (XSCREEN-1)
-#define YMAX (YSCREEN-1)
 
-
-
-
+//============================================
 // Random number generator by Jegge (Tri2s)
-//
-// generates a pseudo random number from 0 to max 
-//
-
-// A 32-bit version
-
-// An 8 bit version
+//============================================
 
 static uint8_t RandomSeedA = 0x11;
 static uint8_t RandomSeedB = 0x0D;
 
+/**
+ * Generates a pseudo random number from 0 to max 
+ */
 uint8_t next_random (uint8_t max) {
 	RandomSeedA = 0x7F * (RandomSeedA & 0x0F) + (RandomSeedA >> 4);
 	RandomSeedB = 0x3C * (RandomSeedB & 0x0F) + (RandomSeedB >> 4);
@@ -185,6 +120,80 @@ void init_random (void) {
 }
 
 
+
+//============================================
+// Sounds!
+//============================================
+
+static byte SONG_INTRO[] = {
+	N_C4,N_8TH,
+	N_E4,N_8TH,
+	N_F4,N_HALF,
+	N_REST,N_QUARTER,
+	N_C4,N_8TH,
+	N_E4,N_8TH,
+	N_G4,N_HALF,
+	N_REST,N_QUARTER,
+	N_F4,N_8TH,
+	N_E4,N_8TH,
+	N_C4,N_HALF,
+	N_END,
+};
+
+static byte SONG_TAPS[] = {
+	N_G3, N_HALF,
+	N_G3, N_8TH,
+	N_C4, N_WHOLE,
+	N_G3, N_HALF,
+	N_C4, N_8TH,
+	N_E4, N_WHOLE,
+	N_END
+};
+
+static byte SONG_WIN[] = {
+	N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
+	N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
+	N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, 
+	N_END
+};
+
+
+static byte DIRECTION_A_NOISE[] = {N_F4, N_16TH, N_END};
+static byte DIRECTION_B_NOISE[] = {N_D4, N_16TH, N_END};
+static byte DIRECTION_C_NOISE[] = {N_E4, N_16TH, N_END};
+static byte DIRECTION_D_NOISE[] = {N_G4, N_16TH, N_END};
+
+static byte CORRECT_NOISE[] = {N_C5, N_16TH, N_D5, N_16TH, N_E5, N_16TH, N_END};
+
+
+
+
+//============================================
+// Arrows!
+//============================================
+
+/* Arrow Directions */
+#define DIRECTION_A 0
+#define DIRECTION_B	1
+#define DIRECTION_C	2
+#define DIRECTION_D	3
+
+
+static byte DIRECTIONS[4] = {
+	DIRECTION_A,
+	DIRECTION_B,
+	DIRECTION_C,
+	DIRECTION_D
+};
+
+/* Array to hold arrow history */
+static byte arrows[100];
+
+
+
+/**
+ * Displays the arrows on the screen
+ */
 void draw_arrow(byte dir, byte clr) {
 	setcolor(clr);
 	if (dir == DIRECTION_A) {
@@ -243,9 +252,11 @@ void draw_arrow(byte dir, byte clr) {
 	}
 }
 
+/**
+ * Draws an arrow to the screen and plays the appropriate noise
+ */
 void show_next_arrow(int cnt) {
 	byte *noise;
-	byte clr;
 	byte dir = arrows[cnt];
 
 	if (dir == DIRECTION_A) {
@@ -257,52 +268,28 @@ void show_next_arrow(int cnt) {
 	else if (dir == DIRECTION_C) {
 		noise = DIRECTION_C_NOISE;
 	}
-	if (dir == DIRECTION_D) {
+	else {
 		noise = DIRECTION_D_NOISE;
 	}
 	
 	
-	    draw_arrow(arrows[cnt], GREEN);
-	    delay_ms(200);
-		playsong(noise);
-		//waitaudio();
-	    delay_ms(200);
-	    delay_ms(200);
-	    cleardisplay();	
-	    delay_ms(200);
+	draw_arrow(arrows[cnt], GREEN);
+	delay_ms(200);
+	playsong(noise);
+	delay_ms(200);
+	delay_ms(200);
+	cleardisplay();	
+	delay_ms(200);
 
 	
 }
 
 
-void startup_screen() {
-	draw_arrow(DIRECTION_A, GREEN);
-	delay_ms(200);
-	delay_ms(200);
-	delay_ms(200);
-	cleardisplay();
-	draw_arrow(DIRECTION_B, GREEN);
-	delay_ms(200);
-	delay_ms(200);
-	delay_ms(200);
-	cleardisplay();
-	draw_arrow(DIRECTION_C, GREEN);
-	delay_ms(200);
-	delay_ms(200);
-	delay_ms(200);
-	cleardisplay();
-	draw_arrow(DIRECTION_D, GREEN);
-	delay_ms(200);
-	delay_ms(200);
-	delay_ms(200);
-	cleardisplay();
-}
-
-
+//============================================
+// Helper functions drawing large digits
+//============================================
 
 void draw_digit_vert_right(int x_shift) {
-	int col1 = 0 + x_shift;
-	int col2 = 1 + x_shift;
 	int col3 = 2 + x_shift;
 	drawpoint(col3, 0);
 	drawpoint(col3, 1);
@@ -314,8 +301,6 @@ void draw_digit_vert_right(int x_shift) {
 
 void draw_digit_vert_left(int x_shift) {
 	int col1 = 0 + x_shift;
-	int col2 = 1 + x_shift;
-	int col3 = 2 + x_shift;
 	drawpoint(col1, 0);
 	drawpoint(col1, 1);
 	drawpoint(col1, 2);
@@ -353,6 +338,9 @@ void draw_digit_horiz_bot(int x_shift) {
 }
 
 
+//============================================
+// Number Screen
+//============================================
 void draw_number(int number, int x_shift) {
 	int col1 = 0 + x_shift;
 	int col2 = 1 + x_shift;
@@ -428,6 +416,40 @@ void draw_number(int number, int x_shift) {
 
 
 
+//============================================
+// Game Screens
+//============================================
+
+/**
+ * Shows the startup screen
+ */
+void startup_screen() {
+	draw_arrow(DIRECTION_A, GREEN);
+	delay_ms(200);
+	delay_ms(200);
+	delay_ms(200);
+	cleardisplay();
+	draw_arrow(DIRECTION_B, GREEN);
+	delay_ms(200);
+	delay_ms(200);
+	delay_ms(200);
+	cleardisplay();
+	draw_arrow(DIRECTION_C, GREEN);
+	delay_ms(200);
+	delay_ms(200);
+	delay_ms(200);
+	cleardisplay();
+	draw_arrow(DIRECTION_D, GREEN);
+	delay_ms(200);
+	delay_ms(200);
+	delay_ms(200);
+	cleardisplay();
+}
+
+
+/**
+ * Shows the game over screen
+ */
 void gameover_screen(int level) {
 	setcolor(RED);
 	draw_number((level % 10), 4);
@@ -437,150 +459,145 @@ void gameover_screen(int level) {
 
 
 
-int
-main(void)
-{
+//============================================
+// Main game logic
+//============================================
+
+int main(void) {
 	init_random();
 	avrinit();
 	int cnt;
 	byte btnDown = 0;
 	byte level = 1;
-
-	arrows[0] = directions[next_random(4)];
-
+	arrows[0] = DIRECTIONS[next_random(4)];
 
 	initswapbuffers();
 	swapinterval(10);		// note: display refresh is 100hz (lower number speeds up game)
 	cleardisplay();
-
 	start_timer1();			// this starts display refresh and audio processing
-	
 	button_init();
-
 	initaudio();			// XXX eventually, we remove this!
 
-	//setwavetable(WT_SINE);
-	playsong(IntroSong);
-
+	playsong(SONG_INTRO);
 	startup_screen();
-
 	delay_sec(1);
 
 
-	//
-	// and now, the game
-	//
-nextlevel:
-    cleardisplay();
-    for(cnt=0; cnt<level; cnt++) {
-		show_next_arrow(cnt);
-    }
-    
-    cnt = 0;
-
-	while(1) {
-
+	//~~~ GOTO: NEXT LEVEL ~~~
+	nextlevel:
 		cleardisplay();
-		handlebuttons();
 		
-		
-		if (!btnDown) {
-
-			if (ButtonA || ButtonB || ButtonC || ButtonD) {
-				btnDown = 1;			
-			}
-
-	
-			if (ButtonA) {
-				draw_arrow(DIRECTION_A, YELLOW);
-				playsong(DIRECTION_A_NOISE);
-				delay_ms(100);
-				if (arrows[cnt] == DIRECTION_A) {
-					cnt++;
-				}
-				else {
-					goto gameover;
-				}
-	
-			}
-			if (ButtonB) {
-				draw_arrow(DIRECTION_B, YELLOW);
-				playsong(DIRECTION_B_NOISE);
-				delay_ms(100);
-				if (arrows[cnt] == DIRECTION_B) {
-					cnt++;
-				}
-				else {
-					goto gameover;
-				}
-	
-			}
-	
-			if (ButtonC) {
-				draw_arrow(DIRECTION_C, YELLOW);
-				playsong(DIRECTION_C_NOISE);
-				delay_ms(100);
-				if (arrows[cnt] == DIRECTION_C) {
-					cnt++;
-				}
-				else {
-					goto gameover;
-				}
-	
-			}
-			if (ButtonD) {
-				draw_arrow(DIRECTION_D, YELLOW);
-				playsong(DIRECTION_D_NOISE);
-				delay_ms(100);
-				if (arrows[cnt] == DIRECTION_D) {
-					cnt++;
-				}
-				else {
-					goto gameover;
-				}
-			
-			}
-			
-
-		
-			if (cnt == level) {
-				if (level == 99) {
-					goto gamewin;
-				}
-				cleardisplay();
-				delay_ms(200);
-				playsong(CORRECT_NOISE);
-				level++;
-				arrows[cnt] = directions[next_random(4)];
-				delay_ms(200);
-				delay_ms(200);
-				goto nextlevel;
-			}
-
+		//show all of the arrows in our history
+		for(cnt=0; cnt<level; cnt++) {
+			show_next_arrow(cnt);
 		}
-		else {
-			if (!ButtonA && !ButtonB && !ButtonC && !ButtonD) {
-				btnDown = 0;
-			}
-		}
-
 		
+		cnt = 0;
+	
+		while(1) {
+			cleardisplay();
+			handlebuttons();
+			
+			//look for button presses and compare to history
+			if (!btnDown) {
 
-    }
-
-gamewin:
-	cleardisplay();
-	//do something;
-	playsong(WIN_NOISE);
-	gameover_screen(level);
-	return (0);
-
-
-gameover:
-	cleardisplay();
-	delay_ms(200);
-	delay_ms(200);
-	playsong(TapsSong);
-	gameover_screen(level);
-	return (0);
+				//make sure we only count long button presses once
+				if (ButtonA || ButtonB || ButtonC || ButtonD) {
+					btnDown = 1;			
+				}
+	
+		
+				if (ButtonA) {
+					draw_arrow(DIRECTION_A, YELLOW);
+					playsong(DIRECTION_A_NOISE);
+					delay_ms(100);
+					if (arrows[cnt] == DIRECTION_A) {
+						cnt++;
+					}
+					else {
+						goto gameover;
+					}
+		
+				}
+				if (ButtonB) {
+					draw_arrow(DIRECTION_B, YELLOW);
+					playsong(DIRECTION_B_NOISE);
+					delay_ms(100);
+					if (arrows[cnt] == DIRECTION_B) {
+						cnt++;
+					}
+					else {
+						goto gameover;
+					}
+		
+				}
+		
+				if (ButtonC) {
+					draw_arrow(DIRECTION_C, YELLOW);
+					playsong(DIRECTION_C_NOISE);
+					delay_ms(100);
+					if (arrows[cnt] == DIRECTION_C) {
+						cnt++;
+					}
+					else {
+						goto gameover;
+					}
+		
+				}
+				if (ButtonD) {
+					draw_arrow(DIRECTION_D, YELLOW);
+					playsong(DIRECTION_D_NOISE);
+					delay_ms(100);
+					if (arrows[cnt] == DIRECTION_D) {
+						cnt++;
+					}
+					else {
+						goto gameover;
+					}
+				
+				}
+				
+	
+				//this is the last arrow in the history, add another to the list?
+				if (cnt == level) {
+					if (level == 99) {
+						goto gamewin;
+					}
+					cleardisplay();
+					delay_ms(200);
+					playsong(CORRECT_NOISE);
+					level++;
+					arrows[cnt] = DIRECTIONS[next_random(4)];
+					delay_ms(200);
+					delay_ms(200);
+					goto nextlevel;
+				}
+	
+			}
+			else {
+				if (!ButtonA && !ButtonB && !ButtonC && !ButtonD) {
+					btnDown = 0;
+				}
+			}
+	
+			
+	
+		}
+		
+	//~~~ GOTO: GAME WIN ~~~
+	gamewin:
+		cleardisplay();
+		//do something;
+		playsong(SONG_WIN);
+		gameover_screen(level);
+		return (0);
+	
+	//~~~ GOTO: GAME OVER ~~~
+	gameover:
+		cleardisplay();
+		delay_ms(200);
+		delay_ms(200);
+		playsong(SONG_TAPS);
+		gameover_screen(level);
+		return (0);
 }
